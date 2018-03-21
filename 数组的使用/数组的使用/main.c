404f976312ab2081d30ee4c1c231f7e21ac7180d
jyz <jyz@myimac7.local>
2018-03-21 21:20:19 +0800

    回去想想哪出错了

--- 数组的使用/数组的使用/main.c
+++ 数组的使用/数组的使用/main.c
@@ -2,20 +2,44 @@
 //  数组的使用
 //  Created by jyz on 2018/3/21.
 #include <stdio.h>
-//需求：输入十个数，对其进行排序
-int main(int argc, const char * argv[]) {
+void answer(){
     int score[10];//创建存这10个数的数组
     int i = 0,j = 0;
     for (i = 0; i < 10; i++) {
         scanf("%d",&score[i]);
     }
-    for (i = 0; i < 10; i++) {
-        for (j = 1; j < 10; j++) {
-            if (score[i] > score[j]) {
+    for (i = 0; i < 9; i++) {
+        for (j = i + 1; j < 10; j++) {
+            if (score[i] < score[j]) {
                 int temp = score[i];
-                
+                score[i] = score[j];
+                score[j] = temp;
             }
         }
     }
+    for (i = 0; i < 10; i++) {
+        printf("%d\t",score[i]);
+    }
+}
+//需求：输入十个数，对其进行排序
+int main(int argc, const char * argv[]) {
+//    int score[10];//创建存这10个数的数组
+//    int i = 0,j = 0;
+//    for (i = 0; i < 10; i++) {
+//        scanf("%d",&score[i]);
+//    }
+//    for (i = 0; i < 10; i++) {
+//        for (j = 1; j < 10; j++) {
+//            if (score[i] < score[j]) {
+//                int temp = score[i];
+//                score[i] = score[j];
+//                score[j] = temp;
+//            }
+//        }
+//    }
+//    for (i = 0; i < 10; i++) {
+//        printf("%d\n",score[i]);
+//    }
+    answer();
     return 0;
 }
