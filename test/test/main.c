51c550b784a0ffbbd809c168f2f9febb1d1638b3
jyz <jyz@myimac7.local>
2018-04-10 20:55:31 +0800

    newChange

--- test/test/main.c
+++ test/test/main.c
@@ -1,101 +1,24 @@
 #include <stdio.h>
-#include <math.h>
-//MARK: 5.1
+//MARK: 求自然数的立方和 >= 1000000
 void demo1(){
-    double a = 0;
-    printf("请输入一个实数:");
-    scanf("%lf",&a);
-    if (a < 0) {
-        a = -a;
+    int i = 0;
+    long sum = 0;
+    while (sum < 1000000) {
+        i++;
+        sum += i * i * i;
     }
-    printf("他的绝对值为：%g\n",a);
+    printf("%d\n", i);
 }
-//MARK: 5.2
+//MARK: 求 pi 的前 100 项乘积
 void demo2(){
-    int a = 0;
-    printf("请输入一个数，用来判断其奇偶性：");
-    scanf("%d",&a);
-    if (a % 2 == 0) {
-        printf("它是偶数\n");
-    }else{
-        printf("他是奇数\n");
+    double pi = 1;
+    for (double n = 2; n <= 100; n += 2) {
+        pi *= (n * n) / ((n - 1) * (n + 1));
     }
-}
-//MARK: 5.3
-void demo3(){
-    double width[3];
-    int i;
-    for (i = 0; i < 3; i++) {
-        printf("请输入第%d个边长：",i + 1);
-        scanf("%lf", &width[i]);
-    }
-    if (width[1] + width[2] > width[0] && width[2] + width[0] > width[1] && width[1] + width[0] > width[2]) {
-        float s = (width[0] + width[1] +width[2])/2;
-        float area = sqrt(s * (s - width[0]) * (s - width[1]) * (s - width[2]));
-        printf("这个三角形的面积是：%g\n",area);
-    } else {
-        printf("这个不是三角形\n");
-    }
-}
-//MARK: 5.4
-void demo4(){
-    int year = 1;
-    double money = 0, allMoney = 0;
-    printf("请输入您所需要存款的本金：");
-    scanf("%lf",&money);
-    printf("请输入您所需要存储的时间：");
-    scanf("%d",&year);
-    switch (year) {
-        case 1:
-            allMoney = money * (1 + 0.0225);
-            printf("%g\n",allMoney);
-            break;
-        case 2:
-            allMoney = money * pow((1 + 0.0243), 2);
-            printf("%g\n",allMoney);
-            break;
-        case 3:
-            allMoney = money * pow((1 + 0.0227), 3);
-            printf("%g\n",allMoney);
-            break;
-        case 5:
-            allMoney = money * pow((1 + 0.0288), 5);
-            printf("%g\n",allMoney);
-            break;
-        case 8:
-            allMoney = money * pow((1 + 0.03), 8);
-            printf("%g\n",allMoney);
-            break;
-        default:
-            printf("无法计算\n");
-            break;
-    }
-}
-//MARK: 5.5
-void demo5(){
-    int year = 0,flag = 0;
-    printf("Enter year:");
-    scanf("%d",&year);
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-        flag = 1;
-    } else {
-        flag = 0;
-    }
-    if (flag == 1) {
-        printf("%d是闰年\n",year);
-    } else {
-        printf("%d不是闰年\n",year);
-    }
+    printf("%g\n", pi * 2);
 }
 int main(int argc, const char * argv[]) {
     demo1();
-    printf("------\n");
     demo2();
-    printf("------\n");
-    demo3();
-    printf("------\n");
-    demo4();
-    printf("------\n");
-    demo5();
     return 0;
 }
---
+++ 指针/指针与数组的关系/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/*
+ 指针与数组之间的关系
+    因为数组的名称与函数的名称本身就是指针。（函数的名称本身也是一种指针。）
+    数组名单独拿出来就是一个地址。
+ */
+void demo1(){
+    int a[10];
+    for (int i = 0; i < sizeof(a) / sizeof(int); i++) {
+        a[i] = i + 1;
+    }
+    // 在这里面，指针变量 p 保存的是数组 a 的首地址。
+    int * p = a;
+    for (int i = 0; i < sizeof(a) / sizeof(int); i++) {
+        // 在输出数组的数值的时候，有两种方法：1、通过数组的下标寻找；2、通过地址寻找。因为数组是在内存中开辟了连续的空间。所以可以直接通过地址的加减即可。（这也就是为啥指针还需要在前面声明数据类型）
+        printf("a[%d] = %d\n", i, *(p + i));
+    }
+}
+// MARK: 函数传递一维数组的问题
+void demo2(int * p){
+    printf("%d\n", p[9]);
+}
+// MARK: 函数传递二位数组
+// 在二维数组中，也可以通过指针来定位数组里的数据
+void demo3(int * s){
+    printf("%d\n", *(s + 5) );
+}
+// MARK: 指针与字符串之间的关系
+void demo4(){
+    char ch[] = "Hello";
+    char * p = "Hello";
+    printf("%s\n", p);
+}
+int main(int argc, const char * argv[]) {
+    demo1();
+    int a[10] = {0};
+    int b[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    demo2(a);// 为什么在这里面我不能调用 demo1 中的 p 指针？只能调用 main 函数里面的数组？
+    demo3(*b);
+    demo4();
+    return 0;
+}
