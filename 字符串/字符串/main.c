112d45067a7fd1748b3d15e5cd742efb569b3903
jyz <jyz@myimac7.local>
2018-04-17 20:43:31 +0800

    newChange

--- 字符串/字符串/main.c
+++ 字符串/字符串/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 /*
  有关字符串的基础知识
  1、在C语言中。字符串的保存是通过数组的方式去保存(不要忘了数组名同样也是指针名)
  2、字符串的长度计算有2种方法
     （1）通过遍历得到
-    （2）通过strlen得到。注意：这个函数是在 String.h 头文件中。
+    （2）通过 strlen 得到。注意：这个函数是在 String.h 头文件中。
  */
 // MARK: 字符串的长度
 void demo1(){
@@ -36,20 +37,45 @@ void demo2(){
     }
 }
 // MARK: 字符串的比较（用 strcmp 函数）
+// 不能直接用这两个字符串的名字来比较字符串中的内容。因为他们比较的并不是字符串里面的内容。
 /*
 strcmp 函数值 = 0 时为两个字符串相等
              < 0 时为 str1 比 str2 短
              > 0 时为 str1 比 str2 长
 */
 void demo3(){
-    char * str = "129047839579";
-    printf("%d\n", strcmp("12904783957", str));
+    char * str = "z";
+    char * s = malloc(sizeof(char));
+    printf("请输入一个字符串：");
+//    scanf("%s", p); 这句话不能调用字符指针，因为我们没有为scanf输入的指针申请存储空间，scanf输入的数据必须也只能存放到实现申请好的存储空间，不然输入的字符可能会覆盖掉代码段或者其他一些地方的数据，引发未知的错误。
+    scanf("%c", s);
+    if (strcmp(str, s) != 0) {
+        printf("Different\n");
+    } else {
+        printf("Same\n");
+    }
+    free(s);
 }
-// MARK: 字符串的拼接
+// MARK: 字符串的拼接（用 strcap ，注意：添加到的数组必须定义数组大小，让内存分配好空间）
+void demo4(){
+    char p[50] = "王尼玛";
+    char * s = "Hello";
+    strcat(p, s);
+    printf("%s\n", p);
+}
+// MARK: 字符串的替换（用 strcpy 指针替换）。前面是被替换的字符串，后面是替换成什么字符串。
+void demo5(){
+    char p[50] = "王尼玛";
+    char * s = "Hello";
+    strcpy(p, s);
+    printf("%s\n", p);
+}
 int main(int argc, const char * argv[]) {
 //    demo1();
 //    printf("--------\n");
 //    demo2();
-    demo3();
+//    demo3();
+    demo4();
+    demo5();
     return 0;
 }
